adde48fb3cda71711e122143ee6c6058d76cc052
skyler <sjgoh@ucsd.edu>
2022-02-23 15:06:04 -0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+/software/CSE/oracle-java-se-14/jdk-14.0.2/bin/javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java MarkdownParse.java
+/software/CSE/oracle-java-se-14/jdk-14.0.2/bin/java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
