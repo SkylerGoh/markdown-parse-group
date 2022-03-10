1ee6d7c74a759a8621090acb184be83381a0a21c
skyler <sjgoh@ucsd.edu>
2022-03-09 21:22:28 -0800

    Update TryCommonMark.java

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
+
 
 class TryCommonMark {
 
@@ -18,7 +21,7 @@ class TryCommonMark {
         node = parser.parse("[a link!](https://something.com) [another link!](some-page.html)");
         LinkCountVisitor linkVisitor = new LinkCountVisitor();
         node.accept(linkVisitor);
-        System.out.println(linkVisitor.linkCount); 
+        System.out.println("Links: " + linkVisitor.linkCount); 
     }
 }
 
@@ -38,13 +41,27 @@ class WordCountVisitor extends AbstractVisitor {
 }
 class LinkCountVisitor extends AbstractVisitor {
     int linkCount = 0;
-
+    ArrayList<String> linkList;
+    public LinkCountVisitor() {
+        linkList = new ArrayList<>();
+    }
     @Override
     public void visit(Link links) {
         // This is called for all Text nodes. Override other visit methods for other node types.
 
         // Count words (this is just an example, don't actually do it this way for various reasons).
         linkCount += 1;
+        // Descend into children (could be omitted in this case because Text nodes don't have children).
+        visitChildren(links);
+    }
+    public ArrayList<String> getLinks(Link links) {
+        // This is called for all Text nodes. Override other visit methods for other node types.
+        if (links == null) {
+            return linkList;
+        }
+        // Count words (this is just an example, don't actually do it this way for various reasons).
+        linkCount += 1;
+        linkList.add(links.getDestination());
         // Descend into children (could be omitted in this case because Text nodes don't have children).
         visitChildren(links);
     }
